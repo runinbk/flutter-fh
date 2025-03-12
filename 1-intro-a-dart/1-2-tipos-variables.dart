ead37149573c306744e1b4e1b602acfac053943f
runinbk <runinbk@gmail.com>
2025-03-12 11:25:49 -0400

    feat: add dynamic type examples to Dart variable demonstration

--- 1-intro-a-dart/1-2-tipos-variables.dart
+++ 1-intro-a-dart/1-2-tipos-variables.dart
@@ -5,7 +5,18 @@ void main () {
   final List<String> abilities = ['impostor']; // abilities es una lista de strings que solo acepta strings como valores
   final sprites = <String>['ditto/front.png', 'ditto/back.png']; // sprites es una lista de strings que solo acepta strings como valores
 
-  
+  // dynamic? == null; // dynamic es un tipo de dato que puede ser cualquier cosa, incluido null (por defecto es null)
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = 1;
+  errorMessage = 1.0;
+  errorMessage = () => true;
+  errorMessage = null;
+  errorMessage = [1, 2, 3, 4];
+  errorMessage = {'name': 'Ditto', 'hp': 100};
+  /* 
+  en este ejemplo, errorMessage es una variable que puede ser cualquier cosa, incluido null por que es de tipo dynamic
+  */
 
   print("""
   $pokemon
@@ -13,5 +24,6 @@ void main () {
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   """);
 }
